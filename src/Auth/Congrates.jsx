c6c82af6d79afe3a30af8811c5a0a5200e84a70c
Teamweblock <manager@weblockinfosoft.com>
2024-12-14 14:33:34 +0530

    `Updated Congrates.jsx, WelcomePageGame1.jsx and Game2Result.jsx files with minor changes`

--- src/Auth/Congrates.jsx
+++ src/Auth/Congrates.jsx
@@ -1,52 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Congrates = () => {
+  const navigate = useNavigate();
+
+  const handleOpen = () => {
+    navigate("/login");
+  };
   return (
     <div className="login-bg-img">
-    <div className="container">
-      <img
-        src="./mainlogo.png"
-        alt=""
-        height={45}
-        width={120}
-        style={{ marginTop: "30px" }}
-      />
-      <div className="row">
-        <div className="col-md-6">
-          <img className="login-img" src="/congrates.svg" alt="Login" />
-        </div>
-        <div className="col-md-6 d-flex  " style={{ marginTop: "50px" }}>
-          <div className="loginform">
-            <div className="all-circle">
-              <div className="login-discription">
-                Not Login your account yet?
+      <div className="container">
+        <img
+          src="./mainlogo.png"
+          alt=""
+          height={45}
+          width={120}
+          style={{ marginTop: "30px" }}
+        />
+        <div className="row">
+          <div className="col-md-6">
+            <img className="login-img" src="/congrates.svg" alt="Login" />
+          </div>
+          <div className="col-md-6 d-flex  " style={{ marginTop: "50px" }}>
+            <div className="loginform">
+              <div className="all-circle">
+                <div className="login-discription">
+                  Not Login your account yet?
+                </div>
+                <div className="both-circle">
+                  <div className="black-circle">
+                    <div className="white-circle"></div>
+                  </div>
+                  <div className="black-circle">
+                    <div className="white-circle"></div>
+                  </div>
+                  <div className="black-circle">
+                    {/* <div className="white-circle"></div> */}
+                  </div>
+                </div>
               </div>
-              <div className="both-circle">
-                <div className="black-circle">
-                  <div className="white-circle"></div>
+              <div className="px-3">
+                <img
+                  src="./congrates1.png"
+                  alt=""
+                  height={200}
+                  width={300}
+                  className="flex justify-center mx-auto"
+                />
+                <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold  text-center mx-auto text-[#ff3a4b]">
+                  Congratulations
+                </h1>
+                <div className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold  text-center mx-auto">
+                  Your password has been changed successfully
                 </div>
-                <div className="black-circle">
-                  <div className="white-circle"></div>
-                </div>
-                <div className="black-circle">
-                  {/* <div className="white-circle"></div> */}
-                </div>
-              </div>
-            </div>
-            <div className="px-3">
-              <img
-                src="./congrates1.png"
-                alt=""
-                height={200}
-                width={300}
-                className="flex justify-center mx-auto"
-              />
-              <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold  text-center mx-auto text-[#ff3a4b]">
-           Congratulations
-              </h1>
-            <div className='text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-2xl font-bold  text-center mx-auto'>Your password has been changed successfully</div>
-              <form className="px-1 lg:py-5">
-                {/* <input
+                <form className="px-1 lg:py-5">
+                  {/* <input
                 className="login-input text-black placeholder:text-black "
                 placeholder="Enter Your Email"
                 type="email"
@@ -56,21 +64,27 @@ const Congrates = () => {
                 required
               /> */}
 
-                <button className=" mt-2 text-white font-bold text-[1.1rem]  bg-[#ff3a4b] py-2 w-full h-[50px] rounded-lg">
-                  Back To Login
-                </button>
-              </form>
+                  <button
+                    className=" mt-2 text-white font-bold text-[1.1rem]  bg-[#ff3a4b] py-2 w-full h-[50px] rounded-lg"
+                    onClick={handleOpen}
+                  >
+                    Back To Login
+                  </button>
+                </form>
 
-              <p className="text-[1rem] font-bold text-center px-2 py-4">Did not receive the email ? Check your spam filter or try another email address </p>
+                <p className="text-[1rem] font-bold text-center px-2 py-4">
+                  Did not receive the email ? Check your spam filter or try
+                  another email address{" "}
+                </p>
 
-              {/* <button   className="flex items-center gap-2 text-[1.2rem] font-semibold mx-auto mt-4"> <span><ChevronLeft size={20}/></span>Back To login</button> */}
+                {/* <button   className="flex items-center gap-2 text-[1.2rem] font-semibold mx-auto mt-4"> <span><ChevronLeft size={20}/></span>Back To login</button> */}
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Congrates
+export default Congrates;
--- src/playgame2/Game2Result.jsx
+++ src/playgame2/Game2Result.jsx
@@ -46,7 +46,7 @@ const Game2Result = () => {
     if (storedlevel) {
       setLevelNumber(storedlevel);
     }
-  }, []);
+  }, [levelNumber]);
   const handleHome = () => {
     navigate("/");
   };
@@ -57,73 +57,31 @@ const Game2Result = () => {
     // Store the updated level back in localStorage
     localStorage.setItem("levelNumber", newLevelNumber);
     // Navigate to the new level with updated levelNumber in the URL
-    navigate(`/game2levelpage?levelNumber=${newLevelNumber}`);
-  };
-
-  const gaugeOptions = {
-    type: "radial-gauge",
-    value: score, // Set the current score value
-    scale: {
-      min: 0, // Minimum value for the gauge
-      max: 100, // Maximum value for the gauge
-    },
-    bar: {
-      enabled: true, // Enable the bar
-      fillMode: "discrete", // Use discrete color segments
-      fills: [
-        { color: "#ff0000", stop: 20 }, // Red for 0-20
-        { color: "#ff6600", stop: 40 }, // Orange for 21-40
-        { color: "#ffcc00", stop: 60 }, // Yellow for 41-60
-        { color: "#99ff66", stop: 80 }, // Light Green for 61-80
-        { color: "#009900", stop: 100 }, // Dark Green for 81-100
-      ],
-      fillOpacity: 1, // Full opacity for the colors
-      stroke: "#000000", // Optional: border color around segments
-      strokeWidth: 1, // Optional: border width
-    },
-    needle: {
-      enabled: true, // Enable the needle
-      length: "90%", // Needle length
-      width: 2, // Needle width
-      color: "#000000", // Needle color
-    },
-    label: {
-      format: `${score}%`, // Display the score as a percentage
-    },
-    segmentation: {
-      // enabled: true,
-      // interval: {
-      //   count: 4,
-      // },
-      spacing: 2,
-    },
+    navigate(`/game2question?levelNumber=${newLevelNumber}`);
   };
 
   return (
     <div className="new-result ">
-         <img
+      <img
         src={logo}
-        
         onClick={handleHome}
-        height={45} width={100}
-      className="cursor-pointer pb-3 absolute sm:top-[10%] sm:left-[10%] flex justify-center items-center mx-auto top-[4%] left-[6%]"
+        height={45}
+        width={100}
+        className="cursor-pointer pb-3 absolute sm:top-[10%] sm:left-[10%] flex justify-center items-center mx-auto top-[4%] left-[6%]"
       />
       <div className="flex  flex-col justify-center items-center  h-screen">
+        <Game2NewResult score={score} levelNumber={levelNumber} />
 
-     
-   
-      <Game2NewResult score={score} levelNumber={levelNumber} />
-
-      {/* Navigation Buttons */}
-      <div className="pt-10 sm:gap-5 gap-3 flex  max-sm:flex-col">
-        <button className="btnhome" onClick={handleHome}>
-          Home
-        </button>
-        <button className="btnhome" onClick={selectLevelPage}>
-          Next Level
-        </button>
+        {/* Navigation Buttons */}
+        <div className="pt-10 sm:gap-5 gap-3 flex  max-sm:flex-col">
+          <button className="btnhome" onClick={handleHome}>
+            Home
+          </button>
+          <button className="btnhome" onClick={selectLevelPage}>
+            Next Level
+          </button>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
